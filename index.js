4695a5b62ec5528dd8545930612e45492e38e8cb
吴东 <511970747@qq.com>
2022-12-19 16:32:11 +0800

    feat

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.ws("/", (ws) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile("./client/index.html", { root: __dirname });
+app.get("/offer", (req, res) => {
+  res.sendFile("./client/offer.html", { root: __dirname });
 });
 
-app.get("/p2p", (req, res) => {
-  res.sendFile("./client/p2p.html", { root: __dirname });
+app.get("/answer", (req, res) => {
+  res.sendFile("./client/answer.html", { root: __dirname });
 });
 
 app.listen(7777);
